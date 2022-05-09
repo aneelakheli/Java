ee3954bb893cd62aa348da828c01a141173d4e59
blacktech-aneel <anil.blacktech@gmail.com>
2022-05-10 00:00:53 +0545

    variable define

---
+++ basic/Variable/VariableExample.java
@@ -0,0 +1,8 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        int a = 70;
+        int b = 90;
+        System.out.println("a and b are integer");
+
+    }
+}
