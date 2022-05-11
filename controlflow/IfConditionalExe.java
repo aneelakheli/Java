fac169f71aec76e6ac45cfd8d0080966533b3224
blacktech-aneel <anil.blacktech@gmail.com>
2022-05-12 01:06:25 +0545

    adding control statement if

---
+++ controlflow/IfConditionalExe.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*   This is an example of simple if statement.
+here the given value is check weather it is odd or not.
+*/
+public class IfConditionalExe {
+    public static void main(String[] args) {
+        int i = 90;
+        if (i % 2 == 1) {
+            System.out.println("The value is odd number");
+        }
+
+    }
+
+}
