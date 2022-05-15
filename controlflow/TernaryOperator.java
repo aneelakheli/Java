dcee390c6f4ce9ad9bf3c609506f12ac2f239a57
blacktech-aneel <anil.blacktech@gmail.com>
2022-05-15 21:07:41 +0545

    Ternary operator

---
+++ controlflow/TernaryOperator.java
@@ -0,0 +1,14 @@
+/* 
+Created By Anil Akheli
+Java Program 
+Ternary operator 
+*/
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int number = 99;
+        String output = (number % 2 == 0) ? "even number" : " odd number";
+        System.out.println(output);
+    }
+
+}
