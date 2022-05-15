e060f6c3533ff587efaa5ee2fdb97f93086ecbab
blacktech-aneel <anil.blacktech@gmail.com>
2022-05-15 19:51:12 +0545

    adding nested if condition

---
+++ controlflow/NestedIF.java
@@ -0,0 +1,19 @@
+/* 
+Created by Anil Akheli
+java program for nested if .
+*/
+public class NestedIF {
+    public static void main(String[] args) {
+        int age = 10;
+        int weight = 90;
+        if (age > 18) {
+            if (weight > 65) {
+                System.out.println("You are eligible to donate Blood");
+            }
+        } else {
+            System.out.println("You are not eligible to donate Blood");
+        }
+
+    }
+
+}
