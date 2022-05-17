597a8dd3071762e2dce85b93caa0b4cce7a7bb1d
anilakheli <kanxoakheli22@gmail.com>
2022-05-17 11:17:15 +0545

    adding while demo example

---
+++ controlflow/WhileDemo.java
@@ -0,0 +1,10 @@
+public class WhileDemo {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 11) {
+            System.out.println(" counting: " + count);
+            count++;
+        }
+    }
+
+}
