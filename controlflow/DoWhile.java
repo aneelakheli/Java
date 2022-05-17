b5a99b7d66e9738943f4e673275601ef785036be
anilakheli <kanxoakheli22@gmail.com>
2022-05-17 11:51:51 +0545

    adding do while

---
+++ controlflow/DoWhile.java
@@ -0,0 +1,10 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println(" counting " + count);
+            count++;
+        } while (count <= 10);
+    }
+
+}
