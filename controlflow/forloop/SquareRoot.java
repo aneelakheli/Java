36cf60c897275f2a60c7fb94c8f7714614dfe603
anilakheli <kanxoakheli22@gmail.com>
2022-05-21 18:36:04 +0545

    adding sqaure

--- controlflow/forloop/SquareRoot.java
+++ controlflow/forloop/SquareRoot.java
@@ -6,12 +6,20 @@ public class SquareRoot {
     public static void main(String[] args) {
         System.out.println("Enter the number");
         Scanner sc = new Scanner(System.in);
+        int sq;
         int num = sc.nextInt();
         for (int i = 2; i <= num / 2; i++) {
-            if (num / i == i) {
-                System.out.println("the number is square");
-                break;
+            // if (num / i == i) {
+            // System.out.println("the number is square");
+            // break;
 
+            // }
+            sq = i * i;
+            if (num == sq) {
+                System.out.println("Square number");
+                break;
+            } else {
+                System.out.println("not square");
             }
         }
     }
