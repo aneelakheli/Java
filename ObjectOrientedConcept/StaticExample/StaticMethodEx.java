2ec767ef1cc59a776ffea87cc244e39ddbcab113
anilakheli <kanxoakheli22@gmail.com>
2022-06-02 17:53:37 +0545

    adding static method example

---
+++ ObjectOrientedConcept/StaticExample/StaticMethodEx.java
@@ -0,0 +1,46 @@
+package ObjectOrientedConcept.StaticExample;
+
+class StaticDemo {
+    String name;
+    int rollNo;
+    // static variable
+    static String cllgName;
+    static int counter = 0;
+
+    public StaticDemo(String name) {
+        this.name = name;
+        this.rollNo = SetRollNo();
+    }
+
+    // getting unique rollno through static variable(counter)
+    static int SetRollNo() {
+        counter++;
+        return counter;
+    }
+
+    // static method
+    static void setCllg(String name) {
+        cllgName = name;
+    }
+
+    void getStaticInfo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("rollNo: " + this.rollNo);
+        // accessing static variable
+        System.out.println("Cllg Name: " + cllgName);
+
+    }
+
+}
+
+public class StaticMethodEx {
+    public static void main(String[] args) {
+        StaticDemo.setCllg("HariRam");
+        StaticDemo sd1 = new StaticDemo("Ram");
+        StaticDemo sd2 = new StaticDemo("Bob");
+
+        sd1.getStaticInfo();
+        sd2.getStaticInfo();
+
+    }
+}
