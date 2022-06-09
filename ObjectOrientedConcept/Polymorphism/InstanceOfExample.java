f45bb1852a49065f2c82a8ceae5216aee5f5f830
anilakheli <kanxoakheli22@gmail.com>
2022-06-09 19:26:13 +0545

    adding instanceof example

---
+++ ObjectOrientedConcept/Polymorphism/InstanceOfExample.java
@@ -0,0 +1,9 @@
+package ObjectOrientedConcept.Polymorphism;
+
+public class InstanceOfExample {
+    public static void main(String args[]) {
+        InstanceOfExample s = new InstanceOfExample();
+        System.out.println(s instanceof InstanceOfExample);// true
+    }
+
+}
