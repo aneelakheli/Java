d99ef9d0db0f5e2d81d20ca9da37c7f696e63a2c
anilakheli <kanxoakheli22@gmail.com>
2022-06-12 22:17:28 +0545

    adding interface Example

--- ObjectOrientedConcept/Abstraction/InterfaceExampleTwo.java
+++ ObjectOrientedConcept/Abstraction/InterfaceExampleTwo.java
@@ -1,18 +1,23 @@
 package ObjectOrientedConcept.Abstraction;
 
-import java.util.Scanner;
-
 public class InterfaceExampleTwo {
     public static void main(String[] args) {
         Bank n = new Nabil();
+        Bank m = new NMB();
         System.out.println("ROI: " + n.rateOfInterest());
+        System.out.println("ROI: " + n.calculateInterest());
+        System.out.println("ROI: " + m.rateOfInterest());
+        System.out.println("ROI: " + m.calculateInterest());
     }
 
 }
 
 interface Bank {
+    float year = 7;
 
     float rateOfInterest();
+
+    float calculateInterest();
 
 }
 
@@ -22,12 +27,20 @@ class Nabil implements Bank {
         return 9.15f;
     }
 
+    public float calculateInterest() {
+        return rateOfInterest() * year;
+    }
+
 }
 
 class NMB implements Bank {
 
     public float rateOfInterest() {
         return 9.7f;
+    }
+
+    public float calculateInterest() {
+        return rateOfInterest() * year;
     }
 
 }
