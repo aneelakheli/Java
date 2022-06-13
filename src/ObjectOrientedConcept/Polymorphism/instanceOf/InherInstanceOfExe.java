9b985553e323cba7a0180b7a5cb22a707b1e31e1
anilakheli <kanxoakheli22@gmail.com>
2022-06-13 17:19:28 +0545

    directory file

---
+++ src/ObjectOrientedConcept/Polymorphism/instanceOf/InherInstanceOfExe.java
@@ -0,0 +1,12 @@
+package ObjectOrientedConcept.Polymorphism.instanceOf;
+
+class InheritanceInstanceOfExericise {
+
+}
+
+public class InherInstanceOfExe extends InheritanceInstanceOfExericise {
+    public static void main(String args[]) {
+        InherInstanceOfExe d = new InherInstanceOfExe();
+        System.out.println(d instanceof InheritanceInstanceOfExericise);// true
+    }
+}
