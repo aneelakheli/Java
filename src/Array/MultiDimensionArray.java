4655fcead2f518b0c7b13eaec59b3467dba4e9e4
anilakheli <kanxoakheli22@gmail.com>
2022-06-14 18:14:08 +0545

    multi dimensional array example

--- src/Array/MultiDimensionArray.java
+++ src/Array/MultiDimensionArray.java
@@ -3,7 +3,7 @@ public class MultiDimensionArray {
         int arr[][] = { {1,2,3},{9,8,7},{3,8,5}};
         for(int i=0;i<3;i++){
             for(int j =0;j<3;j++){
-                System.out.println(arr[i][j]+ " ");
+                System.out.print(arr[i][j]+ " ");
                 
             }
             System.out.println();
