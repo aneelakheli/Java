f4f479375f9ae112d77679545f88f1ad49eb3d86
anilakheli <kanxoakheli22@gmail.com>
2022-07-27 20:10:58 +0545

    adding problem solved question

---
+++ src/ProblemSolve/AddingNinity.java
@@ -0,0 +1,15 @@
+public class AddingNinity {
+    public static void main(String[] args) {
+        int i, j, sum = 102 ;
+
+        int[] A = new int[]{51,71,20,42};
+for(i = 0;i<A.length;i++){
+    for(j = i+1;j<A.length;j++){
+    if(i !=j &&A[i] +A[j] ==91){
+      sum = A[i] +A[j];
+
+    }
+}
+}
+    }
+}
