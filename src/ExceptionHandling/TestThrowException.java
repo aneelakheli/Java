ad2f9ded0709089c311470528e30ec21e87443f9
anilakheli <kanxoakheli22@gmail.com>
2022-11-30 20:33:01 +0545

    throw exception

---
+++ src/ExceptionHandling/TestThrowException.java
@@ -0,0 +1,17 @@
+public class TestThrowException{
+    public static void validate(int age) {  
+        if(age<18) {  
+            //throw Arithmetic exception if not eligible to vote  
+            throw new ArithmeticException("Person is not eligible to vote");    
+        }  
+        else {  
+            System.out.println("Person is eligible to vote!!");  
+        }  
+    }  
+    //main method  
+    public static void main(String args[]){  
+        //calling the function  
+        validate(13);  
+        System.out.println("rest of the code...");    
+  }    
+}
