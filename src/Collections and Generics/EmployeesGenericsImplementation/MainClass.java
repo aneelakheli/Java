bdc7d0c2c3cc035621ddcadc1bbb4cb3c7bb2673
anilakheli <kanxoakheli22@gmail.com>
2022-12-14 18:40:20 +0545

    genetics implementation

--- src/Collections and Generics/EmployeesGenericsImplementation/MainClass.java
+++ src/Collections and Generics/EmployeesGenericsImplementation/MainClass.java
@@ -3,11 +3,20 @@ public class MainClass {
         Manager anil = new Manager("Anil");
         Directors arjn = new Directors("aarjan Akheli");
         Supervisor aabu = new Supervisor("aabiskar Akheli");
+        // Office oracle = new Office("Oracle");
 
-        Office oracle = new Office("Oracle");
-        oracle.addMember(anil);
-        oracle.addMember(arjn);
-        oracle.addMember(aabu);
-        System.out.println(oracle.numEmployee());
+        Office<Manager> manager = new Office<>("Managers");
+        manager.addMember(anil);
+
+        Office<Directors> directors = new Office<>("Directors");
+        directors.addMember(arjn);
+        directors.employeeInformation(directors, 2000000,2);
+
+        manager.employeeInformation(manager, 2000000, 6);
+
+        // oracle.addMember(anil);
+        // oracle.addMember(arjn);
+        // oracle.addMember(aabu);
+        // System.out.println(oracle.numEmployee());
     }
 }
